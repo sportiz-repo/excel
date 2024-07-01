5cb9980802378ecfbe460eef8052292f6bee8c5b
sandeepan.sportiz <sandeepan.sportiz@gmail.com>
2024-07-01 10:24:28 +0530

    Clean up

--- src/main/java/com/example/realation/TableRelationApplication.java
+++ src/main/java/com/example/realation/TableRelationApplication.java
@@ -1,9 +1,5 @@
 package com.example.realation;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TableRelationApplication {
 
 	public static void main(String[] args) {
-		Path path = Paths.get(Paths.get("").toAbsolutePath() + File.separator + "src" + File.separator + "resources"
-				+ File.separator + "main" + File.separator + "participant_excel");
-		System.out.println(Paths.get(Paths.get("").toAbsolutePath() + File.separator + "src" + File.separator + "resources"
-				+ File.separator + "main" + File.separator + "participant_excel"));
-		
 		SpringApplication.run(TableRelationApplication.class, args);
 	}
 
